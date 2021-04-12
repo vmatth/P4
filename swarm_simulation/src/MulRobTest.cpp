25ca7a2816835d77a771f90a0d4db11552b756e3
vmatth <vmatth19@student.aau.dk>
2021-04-12 13:40:27 +0200

    Turtlebot roundround

--- swarm_simulation/src/MulRobTest.cpp
+++ swarm_simulation/src/MulRobTest.cpp
@@ -10,15 +10,24 @@ int main(int argc, char *argv[])
 
     NodeHandle n;
 
-    Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/turtlebot1/cmd_vel", 1);
+    Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/turtlebot1/mobile_base/commands/velocity", 1);
     geometry_msgs::Twist cmd_vel_message;
     cmd_vel_message.angular.z = 1.0;
     cmd_vel_message.linear.x = 1.0;
+
+
+    NodeHandle n2;
+
+    Publisher cmd_vel_pub2 = n.advertise<geometry_msgs::Twist>("/turtlebot2/mobile_base/commands/velocity", 1);
+    geometry_msgs::Twist cmd_vel_message2;
+    cmd_vel_message2.angular.z = 1.0;
+    cmd_vel_message2.linear.x = 1.0;
 
     Rate loop_rate(10);
     while (ok())
     {
         cmd_vel_pub.publish(cmd_vel_message);
+        cmd_vel_pub2.publish(cmd_vel_message2);
         loop_rate.sleep();
     }
 
