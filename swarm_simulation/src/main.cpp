7581b3e124f765a7a43e4c38a6896a466de37196
vmatth <vmatth19@student.aau.dk>
2021-04-13 11:42:59 +0200

    Turtlebot main

--- swarm_simulation/src/main.cpp
+++ swarm_simulation/src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 
+using namespace std;
+using namespace ros;
 
-int main(int argc, char const *argv[])
+class Turtlebot{
+private:
+    NodeHandle n;
+    Publisher cmd_vel_pub;
+    geometry_msgs::Twist cmd_vel_message;
+
+public:
+
+     
+
+};
+
+int main(int argc, char *argv[])
 {
+
+    init(argc, argv, "Multi_Robot_Test");
+
+    Turtlebot t;
+
     std::cout << "Hello World" << std::endl;
     return 0;
 }
